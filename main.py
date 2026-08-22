22ccf43c4a218f956ce8ed60800b162f2d1ad615
dsochamp <mohammedhamdansiddiqui7@gmail.com>
2026-08-22 16:00:23 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import matplotlib.pyplot as plot
+import numpy
+
+img = Image.open(input('Which image would you like to open? '))
+
+scale = int(input('What would you like your scale down to be? e.g. 2 is for 50% scale down '))
+
+img = img.resize((int(img.width / scale),int(img.height / scale)))
+
+greyimg = img.convert('L')
+
+characters = '0|/!@*^&±%$# '
+
+line = ''
+
+for y in range(greyimg.height):
+    for x in range(greyimg.width):
+        index = int(greyimg.getpixel((x,y)) / (255/len(characters) + 1))
+        line += characters[index]
+        line += ' '
+
+    print(line)
+    line = ''
+    
+
+img.show()
